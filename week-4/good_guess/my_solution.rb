3225a8e3149f1e93ff854940e594bae02fd4f909
caromedellin <caromedellin@gmail.com>
2015-10-24 17:28:20 -0400

    changes in structure

---
+++ week-4/good_guess/my_solution.rb
@@ -0,0 +1,14 @@
+# Good Guess
+
+# I worked on this challenge [by myself, with: Jessie Richardson].
+
+
+# Your Solution Below
+
+def good_guess?(guess)
+ guess == 42
+  return true
+ else
+  return false
+end
+
