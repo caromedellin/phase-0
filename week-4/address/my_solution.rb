3c51cbdb14e4b5a3005017fc36b2ec3575375606
caromedellin <caromedellin@gmail.com>
2015-10-24 14:51:33 -0400

    add address solution

---
+++ week-4/address/my_solution.rb
@@ -0,0 +1,11 @@
+# Format an Address
+def make_address (x,y,z,q)
+ 
+  return "You live at " + x + ", in the beautiful city of " + y + ", " + z + ". Your zip is " +q + "."
+
+  
+end
+# I worked on this challenge [by myself, with: ].
+
+
+# Your Solution Below
