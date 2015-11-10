1c370b824cf52b385e391c2a7b9c49c94ff9c6b3
caromedellin <caromedellin@gmail.com>
2015-11-10 16:05:30 -0500

    add structure to solve the challenges

---
+++ week-7/design-basic-game-solo-challenge/my_solution.js
@@ -0,0 +1,42 @@
+// Design Basic Game Solo Challenge
+
+// This is a solo challenge
+
+// Your mission description:
+// Overall mission:
+// Goals:
+// Characters:
+// Objects:
+// Functions:
+
+// Pseudocode
+//
+//
+//
+//
+//
+
+// Initial Code
+
+
+
+
+
+
+// Refactored Code
+
+
+
+
+
+
+// Reflection
+//
+//
+//
+//
+//
+//
+//
+//
+
