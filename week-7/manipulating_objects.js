b5b11e13a35c3a1b1ee9e10025ca181f5d7d0b08
caromedellin <caromedellin@gmail.com>
2015-11-11 16:22:00 -0500

    children as objects

--- week-7/manipulating_objects.js
+++ week-7/manipulating_objects.js
@@ -5,21 +5,28 @@
 // There is a section below where you will write your code.
 
 // DO NOT ALTER THIS OBJECT BY ADDING ANYTHING WITHIN THE CURLY BRACES!
+
+
 var terah = {
   name: "Terah",
   age: 32,
   height: 66,
   weight: 130,
   hairColor: "brown",
-  eyeColor: "brown"
+  eyeColor: "brown",
 }
 // __________________________________________
 // Write your code below.
 
-
-
-
-
+var adam = new Object();
+adam.name = "Adam";
+terah.spouse = adam;
+terah.weight = 125;
+delete terah.eyeColor;
+terah.children = {};
+adam.spouse = terah;
+var carson = new Object();
+terah.children = carson;
 
 
 
